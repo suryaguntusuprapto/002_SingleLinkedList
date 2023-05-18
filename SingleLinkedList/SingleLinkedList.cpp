2f5725c751d00943cea682e63be78aef5a467121
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-05-18 18:28:23 +0700

    SingleLinkedList

--- SingleLinkedList/SingleLinkedList.cpp
+++ SingleLinkedList/SingleLinkedList.cpp
@@ -118,3 +118,60 @@ void searchdata() {
 		cout << "Data Tidak Ditemukan" << endl;
 	}
 }
+
+int main() {
+	int pilihan;
+	do
+	{
+		try {
+			cout << "1. Tambah Data" << endl;
+			cout << "2. Hapus Data" << endl;
+			cout << "3. Tampilkan Data" << endl;
+			cout << "4. Cari Data" << endl;
+			cout << "5. Keluar" << endl;
+			cout << "Pilihan: ";
+			cin >> pilihan;
+			switch (pilihan) {
+				case 1:
+					addNode();
+					cout << "Data Berhasil Ditambahkan" << endl;
+					system("pause");
+					system("cls");
+					break;
+				case 2:
+					if (listempty()) {
+						cout << "List Kosong" << endl;
+						system("pause");
+						system("cls");
+						break;
+					}
+
+					int nim;
+					cout << "Masukkan NIM: ";
+					cin >> nim;
+					if (deletenode(nim)) {
+						cout << "NIM: " << nim << "Berhasil Dihapus" << endl;
+						system("pause");
+						system("cls");
+					}
+					else 
+						cout << "Data tidak ditemukan" << endl;
+						break;
+				case 3:
+					traverse();
+					break;
+				case 4:
+					searchdata();
+					break;
+				case 5:
+					break;
+				default:
+					cout << "Pilihan tidak ada" << endl;
+					break;
+			}
+		}
+		catch (exception e) {
+			cout << "terjadi kesalahan" << endl;
+		}
+	} while (pilihan != 5);
+}
